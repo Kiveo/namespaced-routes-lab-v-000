63b32ff428c1e32e59b84c663c179aeff1dd3ee7
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 19:09:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -68,6 +68,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name, :preferences)
+    params.require(:song).permit(:title, :artist_name)
   end
 end
