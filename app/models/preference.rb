7add3e15bd29da93695087b09c85e21f2d2a10c3
Maverick Tesini <mavericktesini@gmail.com>
2018-08-03 18:42:36 +0000

    automatically backed up by learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -3,9 +3,17 @@ class Preference < ActiveRecord::Base
   def song_sort_order(song, sort_direction) #controls asc or desc?
     if sort_direction.downcase == 'asc'
       song.sort('title asc')
-    elsif sort_direction.downcase == 'desc' 
+    elsif sort_direction.downcase == 'desc'
       song.sort('title desc')
-    end 
+    end
   end
-  
+
+  def artist_sort_order(artists, sort_method) #control default sort order of /artists page
+    if sort_method.downcase == 'desc'
+       artist.sort('name desc')
+     elsif sort_method.downcase == 'asc'
+       artists.sort('name asc')
+     end
+  end
+
 end
